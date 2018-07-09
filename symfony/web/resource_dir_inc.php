695b0e0e70dcaa6cda155300cde29d2dbf49390c
Hung Nguyen <hungnm@vn.ids.jp>
2018-07-09 14:33:32 +0700

    Initial commit

---
+++ symfony/web/resource_dir_inc.php
@@ -0,0 +1,5 @@
+<?php 
+sfConfig::set('sf_web_css_dir_name','webres_5acde3dbd3adc6.90334155/css');
+sfConfig::set('sf_web_js_dir_name','webres_5acde3dbd3adc6.90334155/js');
+sfConfig::set('sf_web_images_dir_name','webres_5acde3dbd3adc6.90334155/images');
+sfConfig::set('ohrm_resource_dir', 'webres_5acde3dbd3adc6.90334155');
